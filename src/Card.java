a7289309a0a2958f89200430e039967e7f0accae
7qrous <louis.develog@gmail.com>
2024-12-19 18:49:20 +0900

    update

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+
+public class Card {
+	int power;
+	int type;
+
+	public Card(int power, int type) {
+		this.power = power;
+		this.type = type;
+	}
+	public Card() {
+		
+	}
+}
---
+++ src/Test.java
@@ -0,0 +1,28 @@
+
+public class Test {
+	public int compare(Card c1, Card c2) {
+		int typeResult = compareType(c1.type, c2.type);
+		int compareResult = comparePower(c1.power, c2.power);
+		if (typeResult == 0) {
+			return compareResult;
+		} else {
+			return typeResult;
+		}
+	}
+
+	public int comparePower(int p1, int p2) {
+
+		if (p1 > p2) {
+			return 1;
+		} else if (p1 == p2) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+
+	public int compareType(int t1, int t2) {
+		int[][] typeTable = new int[][] { { 0, 1, 0, -1 }, { -1, 0, 1, 0 }, { 0, -1, 0, 0 }, { 1, 0, -1, 0 } };
+		return typeTable[t1][t2];
+	}
+}
